7517061609b0b32905036a654cfb8c1c2477b5b5
faye <0hachi@pm.me>
2022-12-30 01:05:20 +0100

    fix: checkout script copying wrongly

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -2,11 +2,10 @@
 
 make clean build
 git checkout build
-	
-cp -r dist/* .
-	
-git add .
+
+git checkout main -- dist
+git add dist/**/*
 git commit -m "build: $(date) (automated commit)" -m "$(git log -1 --pretty=%B)"
-	
+
 git push origin build
 git checkout main
